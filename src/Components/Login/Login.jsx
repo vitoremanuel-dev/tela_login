54cba58e538027163c3ab5e35cff0a13d59542af
Vítor Emanuel <vitoremanuel.dev@gmail.com>
2024-05-03 14:01:41 -0300

    Projeto finalizado - Tela de Login

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,55 @@
+import "./Login.css";
+
+import { useState } from "react";
+import { FaUser, FaLock } from "react-icons/fa";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <h1>Acesse o sistema</h1>
+        <div className="input-field">
+          <input
+            type="email"
+            placeholder="E-mail"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <FaUser className="icon" />
+        </div>
+        <div className="input-field">
+          <input
+            type="password"
+            placeholder="Senha"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <FaLock className="icon" />
+        </div>
+
+        <div className="recal-forget">
+          <label>
+            <input type="checkbox" />
+            Lembrar de mim
+          </label>
+          <a href="#">Esqueci a senha</a>
+        </div>
+
+        <button>Entrar</button>
+
+        <div className="signup-link">
+          <p>
+            Não tem uma conta? <a href="#">Registrar</a>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
